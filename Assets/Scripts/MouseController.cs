ce12b160dc686448087e73c35421cbb8c4a8948f
NicEastvillage <nicoesterby@gmail.com>
2016-11-02 11:08:39 +0100

    Move indicator added

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+    [Header("Scene and settings")]
+    public Camera camera;
+    public GameObject moveIndicatorPrefab;
+
+    [Header("Variables")]
+    public Transform moveIndicator;
+
+    public static MouseController instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+	    if (moveIndicator == null)
+        {
+            moveIndicator = (Instantiate(moveIndicatorPrefab, transform) as GameObject).transform;
+            moveIndicator.gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            moveIndicator.transform.position = hitInfo.collider.gameObject.transform.position + hitInfo.normal * 0.51f;
+            moveIndicator.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+
+            moveIndicator.gameObject.SetActive(true);
+
+        } else
+        {
+            moveIndicator.gameObject.SetActive(false);
+        }
+	}
+}
